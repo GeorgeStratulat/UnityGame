fffbbc9ca8fcb93f1d2a882abfe1d634b8c449f5
DESKTOP-88IF2P3\George Stratulat <george.stratulat10@gmail.com>
2017-05-23 14:41:17 +0200

    commit

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverScript: MonoBehaviour {
+
+    int score = 0;
+	// Use this for initialization
+	void Start () {
+        score = PlayerPrefs.GetInt("Score");
+	}
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width / 2 - 40, 30, 80, 30), "Game Over");
+
+        GUI.Label(new Rect(Screen.width / 2 - 40, 300, 80, 30), "Score: " + score);
+        if(GUI.Button(new Rect(Screen.width / 2 - 30, 350, 60, 30), "Retry?"))
+        {
+            Application.LoadLevel(0);
+        }
+    }
+}
